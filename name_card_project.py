3b0d9086ac356d28384c75e4348c5e5043b8df68
choijihee0619 <jihee980619@naver.com>
2025-03-10 17:48:50 +0900

    명함수정

--- name_card_project.py
+++ name_card_project.py
@@ -1,6 +1,3 @@
-## 명함관리 프로그램 작성
-# 1. 명함입력, 2. 명함수정, 3. 명함삭제, 4. 명함목록보기, 5. 종료
-
 import sys
 
 display = '''
@@ -9,7 +6,7 @@ display = '''
 -------------------------------------------------------------
 메뉴를 선택하세요 >>> '''
 
-business_card = []
+business_card = [['최지희','01050259184','학생']]
 
 menu = ''
 while True:
@@ -31,35 +28,21 @@ while True:
     elif menu == '2':
         print('/n 명함수정')
         name = input("수정할 명함의 이름을 입력하세요: ").strip()
-
-        index = -1
-        a = 0
-        while a < len(business_card):
-            if business_card[a][0] == name:
-                index = a
-                break
-            a += 1
-
-        if index == -1:
-            print("해당 명함이 없습니다.")
-            continue
         
-        print(f"현재 정보: {business_card[index]}")
-        print("수정할 항목을 선택하세요: 1. 이름  2. 전화번호  3. 소속")
-        choice = input("번호 입력: ").strip()
+        check = 0
+        for card in business_card:
+            if name == card[0]:
+                while True:   
+                    choice = int(input('수정할 값을 선택하세요>>> 1. 이름 2. 전화번호 3. 소속'))
+                    if choice in (1,2,3):
+                        card[choice - 1] = input('수정할 값: ')
+                        check = 1
+                        break
+        if check == 0:
+            print('데이터가 없습니다')       
+                        
 
-        if choice == "1":
-            business_card[index][0] = input("새로운 이름: ").strip()
-        elif choice == "2":
-            business_card[index][1] = input("새로운 전화번호: ").strip()
-        elif choice == "3":
-            business_card[index][2] = input("새로운 소속: ").strip()
-        else:
-            print("잘못된 입력입니다.")
-            continue
-        
-        print("명함이 수정되었습니다.")
-        print(f"수정된 정보: {business_card[index]}")
+
 
     elif menu == '3':
         print('명함삭제')
